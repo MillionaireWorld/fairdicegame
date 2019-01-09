03353f8d5191790e145635e949cf1c4a8eafda60
millionaireworld <millionaireworld>
2019-01-09 16:55:27 +0800

    * random fair dice game roll

---
+++ README.md
@@ -0,0 +1,77 @@
+# Millionaire World
+  A fair blockchain game based on EOS.
+
+## why we are fair
+  In the old times, when we play a traditional dice game, the game fairness is usually endorsed by the game owner's reputation. If an extremely unlucky player loses 10 times in a row, there is no way for him to tell if he is just being unlucky or not.
+
+With the help of the blockchain technology, now we have a way to design an online provably fair dice game technically. Blockchain is a transparent, tamper-resistant online ledger that every one can audit.
+
+We are glad to see dozens of online dice games popping up in the EOS community recently. It's a pity that most of them are not sig resistant, still leaving inapparent potential backdoors for rigged betting, which is risky and can be largely improved. However, these close sourced dice games did help many rookies embrace the blockchain and get to know the beauty of the EOS world. Their efforts should not be forgotten and we appreciate their value added to the EOS ecology.
+
+Hereby our Millionaire team would like to share the design for our FairDice game and enlighten those who are interested to unleash the power of DApps together.
+
+The key point is that the game has to provide a kind of combined randomness that no single party could control alone.
+
+## 1. Randomness is the key
+
+The key point is that the game has to provide a kind of combined randomness that no single party could control alone.
+
+| ["How can I generate random numbers inside a smart contract?"](https://eosio.stackexchange.com/questions/41/how-can-i-generate-random-numbers-inside-a-smart-contract) |
+| :----------------------------------------------------------: |
+| ![image-20180913144954664](https://raw.githubusercontent.com/Dappub/fairdicegame/master/img/bm.png) |
+
+Just like what BM answered in the above question on StackExchange, for a two-party (```player```-```house```) dice game, the key point to provide fairness or rig-resistant randomness requires a 3 step process:
+
+> 1. Both parties  (  ```player``` & ```house```  ) commit hashes of their seeds:
+>
+>    ```hash(player_seed)```  &   ```hash(house_seed)```
+>
+> 2. Both parties reveal their ```hash(player_seed)```  &  ```hash(house_seed)```
+>
+> 3. ```Roll``` = ```hash(player_seed + house_seed)```
+
+The final ```Roll``` is random and it's a kind of combined randomness based on both ```house``` &&  ```player``` 's input (```house_seed```  &  ```player_seed```)
+
+## 2. Sequence Diagram: optimized "FairDice" random dice roll process
+
+Now we can give it a shot to start building our own blockchain-powered dice game. Compared with other blockchain infrastructure, EOS is preferred here as it's fast and provides good user experience.
+
+In fact, to optimize the whole process, it's unnecessary for ```player``` to upload both ```player_hash``` & ```player_seed``` in two steps.
+
+In other words, since the ```house``` has already placed his dark card (```house_hash```) on the table and not able to change it during the bet, the ```player``` could just place his open card (```player_seed```) on the table directly, instead of placing his dark card (```player_hash```) on the table and flipping the card (```player_seed```) in two steps.
+
+Therefore, we have optimized the whole process and improved user experience as below:
+
+| Sequence Diagram: optimized "FairDice" random dice roll process |
+| :----------------------------------------------------------: |
+| ![image-20180915125933975](https://raw.githubusercontent.com/Dappub/fairdicegame/master/img/optimized.png) |
+
+In step 1, since a "referral bonus" is introduced in the game, the ```referrer``` along with the```ROLL under to win``` will be sent to the ```house```.
+
+In step 2, the ```house``` creates a new ```house_seed``` for each bet and keeps it a secret until step 9.
+
+In step 3, the ```house``` calculates the corresponding ```house_hash``` =  ```hash(house_seed)```.
+
+In step 4, the ```house``` creates ```house_sign``` = ```sign(ROLL_under, hash(house_seed), expiration_time, referrer)```.
+
+In step 5, the ```house``` sends ```house_hash```, ```house_sign```, ... to the ```player```.
+
+In step 6, the ```player``` creates his ```player_seed```.
+
+In step 7, the ```player``` uploads the ```house_sign``` & ```player_seed``` to the EOS blockchain.
+
+In step 8, the smart contract deployed on EOS checks to prevent ```player``` from replay attacking the previous ```house_seed``` by cheating in the ```house_sign```.
+
+In step 9, the ```house``` uploads the ```house_seed```.
+
+In step 10, the smart contract verifies the ```house_seed``` to see if it matches with the ```house_hash``` so as to prevent ```house``` from swapping the ```house_seed``` sneakingly.
+
+In step 11, the smart contract calculates the final ```roll``` num based on the ```player_seed``` & ```house_seed```.
+
+## 3.Millionare World random test result
+
+We have run this test for 1 billion times.And the result is highly consistent with the theoretical result.And the result is the same as
+
+this post:http://bbs.xmfish.com/read-htm-tid-3216284.html .You can also run this random roll test to verify it.
+
+  
---
+++ randomtest.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <openssl/rand.h>
+#include <limits>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+// copied from boost https://www.boost.org/
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v) {
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+std::string to_hex(const char* d, uint32_t s) {
+    std::string r;
+    const char* to_hex = "0123456789abcdef";
+    uint8_t* c = (uint8_t*)d;
+    for (uint32_t i = 0; i < s; ++i)
+        (r += to_hex[(c[i] >> 4)]) += to_hex[(c[i] & 0x0f)];
+    return r;
+}
+
+uint8_t compute_random_roll(unsigned char* seed1, unsigned char* seed2, uint8_t (&random_roll)[6]) {
+    size_t hash = 0;
+    hash_combine(hash, to_hex((char *)seed1, sizeof(seed1)));
+    hash_combine(hash, to_hex((char *)seed2, sizeof(seed2)));
+    random_roll[0] = hash % 6 + 1;
+    hash_combine(hash, to_hex((char *)(seed1), sizeof(seed1)));
+    hash_combine(hash, to_hex((char *)(seed2), sizeof(seed2)));
+    random_roll[1] = hash % 6 + 1;
+    hash_combine(hash, to_hex((char *)(seed1), sizeof(seed1)));
+    hash_combine(hash, to_hex((char *)(seed2), sizeof(seed2)));
+    random_roll[2] = hash % 6 + 1;
+    hash_combine(hash, to_hex((char *)(seed1), sizeof(seed1)));
+    hash_combine(hash, to_hex((char *)(seed2), sizeof(seed2)));
+    random_roll[3] = hash % 6 + 1;
+    hash_combine(hash, to_hex((char *)(seed1), sizeof(seed1)));
+    hash_combine(hash, to_hex((char *)(seed2), sizeof(seed2)));
+    random_roll[4] = hash % 6 + 1;
+    hash_combine(hash, to_hex((char *)(seed1), sizeof(seed1)));
+    hash_combine(hash, to_hex((char *)(seed2), sizeof(seed2)));
+    random_roll[5] = hash % 6 + 1;
+
+    /*
+    std::ofstream outFile;
+    outFile.open("bobingresult.csv", std::ios::app);
+    outFile << int(random_roll[0]) << ',' << int(random_roll[1]) << ',' << int(random_roll[2]) << ',' << int(random_roll[3]) << ','
+    << int(random_roll[4]) << ',' << int(random_roll[5]) << '\n';
+    outFile.close();
+    */
+    return 0;
+
+}
+
+bool GetRandomSeed(unsigned char &rnd, int num){
+    int chunk=num;
+    int r;
+    r = RAND_bytes((unsigned char *)&rnd, chunk);
+    if(r <= 0 ){
+        return false;
+    }else{
+        return true;
+    }
+
+}
+
+void compute_result(uint8_t (&random_roll)[6], int32_t (&result)[13]) {
+    uint8_t one_count = 0;
+    uint8_t two_count = 0;
+    uint8_t three_count = 0;
+    uint8_t four_count = 0;
+    uint8_t five_count = 0;
+    uint8_t six_count = 0;
+    for(int i = 0; i < sizeof(random_roll); i++) {
+        uint8_t roll_number = random_roll[i];
+        switch(roll_number){
+            case 1:
+                one_count++;
+                break;
+            case 2:
+                two_count++;
+                break;
+            case 3:
+                three_count++;
+                break;
+            case 4:
+                four_count++;
+                break;
+            case 5:
+                five_count++;
+                break;
+            case 6:
+                six_count++;
+                break;
+            default:
+                printf("roll number must be between 1 and 6\n");
+        }
+    }
+// calculate result
+    if (six_count == 6 || five_count == 6 || three_count == 6 || two_count == 6) {
+        //guandengqiang
+        result[0]++;
+    }
+    else if(four_count == 6){
+        //liubeihong
+        result[1]++;
+    }
+    else if(four_count == 4 && one_count == 2){
+        //CHAJINHUA;
+        result[2]++;
+    }
+    else if(one_count == 6){
+        //biandijin
+        result[3]++;
+    }
+    else if(four_count == 5){
+        //WUHONG
+        result[4]++;
+    }
+    else if(six_count == 5 || five_count == 5 || three_count == 5 || two_count == 5 || one_count == 5){
+        //WUZIDENGKE
+        result[5]++;
+    }
+    else if(four_count == 4){
+        //SIDIANHONG
+        result[6]++;
+    }
+    else if(one_count == 1 && two_count == 1 && three_count == 1 && four_count == 1 && five_count == 1 && six_count == 1){
+        //DUITANG
+        result[7]++;
+    }
+    else if(four_count == 3){
+        //SANHONG
+        result[8]++;
+    }
+    else if(three_count == 4 || two_count == 4 || one_count == 4 || five_count == 4 || six_count == 4){
+        //SIJIN
+        result[9]++;
+    }
+    else if(four_count == 2){
+        //ERJU
+        result[10]++;
+    }
+    else if(four_count == 1){
+        //YIXIU
+        result[11]++;
+    }
+    else{
+        //FAPAI
+        result[12]++;
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    unsigned char *seed1 = (unsigned char *)malloc(32);
+    unsigned char *seed2 = (unsigned char *)malloc(16);
+    uint8_t random_roll[6] = {0};
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+    int five = 0;
+    int six = 0;
+    int result[13] = {0};
+    int count = 100000000;
+    for(int index = 0; index<count; index++) {
+        if (GetRandomSeed(*seed1, 32)) {
+            /*
+            unsigned char *p = seed1;
+
+            for(int count=0; count<32 ; count++)
+            {
+                printf("%02X",*(p+count));
+            }
+            printf("\n");
+             */
+        }
+
+        if (GetRandomSeed(*seed2, 16)) {
+            /*
+            unsigned char *p = seed2;
+            for(int count=0; count<16 ; count++)
+            {
+                printf("%02X",*(p+count));
+            }
+            */
+        }
+        compute_random_roll((seed1), (seed2), random_roll);
+
+        compute_result(random_roll, result);
+
+        for (int i = 0; i < 6; i++) {
+            int number = random_roll[i];
+            switch (number) {
+                case 1:
+                    one++;
+                    break;
+                case 2:
+                    two++;
+                    break;
+                case 3:
+                    three++;
+                    break;
+                case 4:
+                    four++;
+                    break;
+                case 5:
+                    five++;
+                    break;
+                case 6:
+                    six++;
+                    break;
+                default:
+                    printf("num must be 1-6\n");
+                    break;
+            }
+        }
+    }
+    printf("one:%d two:%d three:%d four:%d five:%d six:%d total count:%d \n", one, two, three, four, five, six, count);
+    printf("关灯抢: %d 六杯红: %d  插金花: %d 遍地锦: %d 五红: %d 五子登科: %d 四点红: %d 对堂: %d 三红: %d 四进: %d 二举: %d \
+    一秀: %d 罚牌: %d \n",result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7],result[8],result[9],\
+    result[10],result[11],result[12]);
+}
